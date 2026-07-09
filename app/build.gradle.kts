16011ef334ffc933abf23d16c584268dd6cacbda
WanguiThiongo <wangoithiongo@gmail.com>
2026-07-09 11:59:11 +0300

    Google ads

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+
+    implementation("com.google.android.gms:play-services-ads:23.0.0")
 }
--- app/src/main/java/com/example/wanguiwellnessapp/MainActivity.kt
+++ app/src/main/java/com/example/wanguiwellnessapp/MainActivity.kt
@@ -7,12 +7,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 class MainActivity : AppCompatActivity() {
+
+    private var mInterstitialAd: InterstitialAd? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        loadInterstitialAd()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -31,6 +40,8 @@ class MainActivity : AppCompatActivity() {
         healthyRecipes.setOnClickListener {
             val newpage = Intent(applicationContext, HealthyRecipesActivity::class.java)
             startActivity(newpage)
+
+            showInterstitialAd()
         } //end intent
 
         nutritionAdvice.setOnClickListener {
@@ -51,11 +62,46 @@ class MainActivity : AppCompatActivity() {
         startExercise.setOnClickListener {
             val newpage = Intent(applicationContext, StartExerciseActivity::class.java)
             startActivity(newpage)
+
+            showInterstitialAd()
         }// end intent
 
         dailyMotivation.setOnClickListener {
             val newpage = Intent(applicationContext, DailyMotivationActivity::class.java)
             startActivity(newpage)
         }//end intent
+
+//        Below we implement our ad
+        MobileAds.initialize(this)
+        val adView = findViewById<AdView>(R.id.adview)
+        val adRequest = com.google.android.gms.ads.AdRequest.Builder().build()
+        adView.loadAd(adRequest)
+    }
+
+
+    fun loadInterstitialAd() {
+        val adRequest = AdRequest.Builder().build()
+        //Requests interstitial ads
+        InterstitialAd.load(
+            this,
+            "ca-app-pub-3940256099942544/1033173712", // Test ID
+            adRequest,
+            object : InterstitialAdLoadCallback() {
+
+                override fun onAdLoaded(ad: InterstitialAd) {
+                    mInterstitialAd = ad
+                }
+
+                override fun onAdFailedToLoad(error: LoadAdError) {
+                    mInterstitialAd = null
+                }
+            }
+        )
+    }
+    //Function checks if ad already running not to run another one and overlap - which is wrong
+    fun showInterstitialAd() {
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(this)
+        }
     }
 }
